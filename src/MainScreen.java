7b953183abec7154e59168efc7dcdb4ab774adef
Karlos Helton Braga <karlos.hbraga@gmail.com>
2020-11-18 15:11:12 -0300

    Start Interface

---
+++ src/MainScreen.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class MainScreen extends JFrame{
+    private JPanel panel1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField textField1;
+
+
+
+    /*
+    public Form() {
+        comboBox1.addItem("X");
+        comboBox1.addItem("Y");
+
+        comboBox1.addActionListener(actionEvent -> {
+            String command = actionEvent.getActionCommand();
+            if (command.equals("comboBoxChanged")) {
+                Object item = comboBox1.getSelectedItem();
+                System.out.println(item);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Form");
+        frame.setContentPane(new Form().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    */
+}
