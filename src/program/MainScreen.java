fe6ee1f77fc7abc10c56de20a1609f1f778d6a34
Karlos Helton Braga <karlos.hbraga@gmail.com>
2020-11-30 01:33:01 -0300

    Make form use real converter classes

--- src/program/MainScreen.java
+++ src/program/MainScreen.java
@@ -1,46 +1,144 @@
 package program;
 
-import converters.LitreConverter;
-import converters.MilliLitreConverter;
+import converters.AbstractConverter;
+import converters.MeasureType;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MainScreen extends JFrame{
     private JPanel panel1;
     private JTextField textField2;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
+    private JComboBox<AbstractConverter> comboBox1;
+    private JComboBox<AbstractConverter> comboBox2;
     private JTextField textField1;
+
+    private Map<MeasureType, List<AbstractConverter>> converters = new TreeMap<>();
 
     public MainScreen(){
         File f = new File("src/converters");
         String[] pathnames = f.list();
-        for(String pathname:pathnames){
-            comboBox1.addItem(pathname);
+
+        if (pathnames == null) {
+            printError("converters folder not found.");
+            return;
         }
-        comboBox1.addItem("Litro");
-        comboBox2.addItem("Mililitro");
+
+        for(String pathname:pathnames){
+            String name = pathname.replace(".java", "");
+            AbstractConverter converter = newConverter(name);
+
+            if (converter == null) {
+                continue;
+            }
+
+            comboBox1.addItem(converter);
+
+            MeasureType type = converter.type();
+            if (!converters.containsKey(type)) {
+                converters.put(type, new ArrayList<>());
+            }
+            converters.get(type).add(converter);
+        }
+
+        fillComboToUnit();
+
         comboBox1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String command = actionEvent.getActionCommand();
                 if (command.equals("comboBoxChanged")) {
-                    String originalValue = textField1.getText();
-                    float value = Float.parseFloat(originalValue);
-                    LitreConverter litreConverter = new LitreConverter();
-                    MilliLitreConverter milliLitreConverter = new MilliLitreConverter();
-                    float basicUnit = litreConverter.toBasicUnit(value);
-                    float newValue = milliLitreConverter.fromBasicUnit(basicUnit);
-                    textField2.setText(Float.toString(newValue));
+                    updateResult();
+                    fillComboToUnit();
                 }
             }
         });
-        comboBox2.addKeyListener(new KeyAdapter() {
+
+        comboBox2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String command = actionEvent.getActionCommand();
+                if (command.equals("comboBoxChanged")) {
+                    updateResult();
+                }
+            }
         });
+        textField1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                updateResult();
+            }
+        });
+    }
+
+    private void fillComboToUnit() {
+        comboBox2.removeAllItems();
+
+        AbstractConverter fromUnit = fromUnit();
+        if (fromUnit == null)
+            return;
+
+        MeasureType type = fromUnit.type();
+
+        for (AbstractConverter toUnit : converters.get(type)) {
+            if (fromUnit != toUnit) {
+                comboBox2.addItem(toUnit);
+            }
+        }
+    }
+
+    private AbstractConverter newConverter(String name) {
+        try {
+            boolean isConverter = name.endsWith("Converter");
+            boolean isMother = name.startsWith("Abstract");
+
+            if (!isConverter || isMother)
+                return null;
+
+            return (AbstractConverter) Class.forName("converters." + name).newInstance();
+        } catch (InstantiationException e) {
+            printError("Class " + name + " cannot be initialized.");
+            return null;
+        } catch (IllegalAccessException e) {
+            printError("It was not possible to access class " + name + ".");
+            return null;
+        } catch (ClassNotFoundException e) {
+            printError("Class " + name + " was not found at package converters.");
+            return null;
+        }
+    }
+
+    private void printError(String message) {
+        System.out.println(message);
+    }
+
+    private void updateResult() {
+        String typedValue = textField1.getText();
+        float value = Float.parseFloat(typedValue);
+
+        AbstractConverter originalUnit = fromUnit();
+        AbstractConverter destinyUnit = toUnit();
+
+        float basicUnit = originalUnit.toBasicUnit(value);
+        float newValue = destinyUnit.fromBasicUnit(basicUnit);
+
+        textField2.setText(Float.toString(newValue));
+    }
+
+    private AbstractConverter fromUnit() {
+        return (AbstractConverter) comboBox1.getSelectedItem();
+    }
+
+    private AbstractConverter toUnit() {
+        return (AbstractConverter) comboBox2.getSelectedItem();
     }
 
     public static void main(String[] args) {
@@ -50,21 +148,4 @@ public class MainScreen extends JFrame{
         frame.pack();
         frame.setVisible(true);
     }
-
-
-
-    /*
-    public Form() {
-        comboBox1.addItem("X");
-        comboBox1.addItem("Y");
-
-        comboBox1.addActionListener(actionEvent -> {
-            String command = actionEvent.getActionCommand();
-            if (command.equals("comboBoxChanged")) {
-                Object item = comboBox1.getSelectedItem();
-                System.out.println(item);
-            }
-        });
-    }
-    */
 }
